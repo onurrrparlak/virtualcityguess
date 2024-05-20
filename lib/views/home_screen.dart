067d0323e668437f11c450b0997dabcde45288f7
Onur Parlak <onurrrparlak@gmail.com>
2024-05-21 00:38:39 +0300

    look

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -12,63 +12,72 @@ class _HomeScreenState extends State<HomeScreen> {
   String _playerName = '';
   String _roomId = '';
 
- void _createRoom() async {
-  if (_playerName.isNotEmpty) {
-    String roomId = await _firestoreService.createRoom();
-    await _firestoreService.joinRoom(roomId, _playerName);
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => MapScreen(roomId: roomId, playerName: _playerName, isHost: true,),
-      ),
-    );
-  }
-}
-
-
-
-void _joinRoom() async {
-  if (_roomId.isNotEmpty && _playerName.isNotEmpty) {
-    try {
-      await _firestoreService.joinRoom(_roomId, _playerName);
+  void _createRoom() async {
+    if (_playerName.isNotEmpty) {
+      String roomId = await _firestoreService.createRoom();
+      await _firestoreService.joinRoom(roomId, _playerName);
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => MapScreen(roomId: _roomId, playerName: _playerName, isHost: false),
+          builder: (context) => MapScreen(
+            roomId: roomId,
+            playerName: _playerName,
+            isHost: true,
+          ),
         ),
       );
-    } on Exception catch (e) {
-      if (e.toString() == 'Exception: Room does not exist') {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Room does not exist'),
-        ));
-      } else if (e.toString() == 'Exception: Game has already started, you cannot join.') {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Game has already started, you cannot join.'),
-        ));
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('An error occurred'),
-        ));
+    }
+  }
+
+  void _joinRoom() async {
+    if (_roomId.isNotEmpty && _playerName.isNotEmpty) {
+      try {
+        await _firestoreService.joinRoom(_roomId, _playerName);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MapScreen(
+              roomId: _roomId,
+              playerName: _playerName,
+              isHost: false,
+            ),
+          ),
+        );
+      } on Exception catch (e) {
+        if (e.toString() == 'Exception: Room does not exist') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Room does not exist'),
+            ),
+          );
+        } else if (e.toString() ==
+            'Exception: Game has already started, you cannot join.') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Game has already started, you cannot join.'),
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('An error occurred'),
+            ),
+          );
+        }
       }
     }
   }
-}
-
-
-
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Virtual City Guess'),
+        elevation: 0,
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.symmetric(horizontal: 0.1 * screenHeight),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -80,6 +89,7 @@ void _joinRoom() async {
                 });
               },
             ),
+            SizedBox(height: 0.05 * screenHeight),
             TextField(
               decoration: InputDecoration(labelText: 'Room ID'),
               onChanged: (value) {
@@ -88,11 +98,12 @@ void _joinRoom() async {
                 });
               },
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 0.1 * screenHeight),
             ElevatedButton(
               onPressed: _createRoom,
               child: Text('Create Room'),
             ),
+            SizedBox(height: 0.05 * screenHeight),
             ElevatedButton(
               onPressed: _joinRoom,
               child: Text('Join Room'),
